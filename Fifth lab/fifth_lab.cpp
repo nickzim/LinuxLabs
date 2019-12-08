2a0420c56904cc9c96308651820a4efea4b3b945
NickZim <nicks.serge@gmail.com>
2019-12-08 19:58:22 +0300

    Fix 5 & 6

--- Fifth lab/fifth_lab.cpp
+++ Fifth lab/fifth_lab.cpp
@@ -9,11 +9,11 @@ void Signal_handler(int signal_num){
 	switch(signal_num){
 		case SIGFPE: {
 			cout << "Ошибка: неверная операция\n";
-			kill(getpid(), SIGHUP);
+			exit(1);
 		}
 		case SIGSEGV: {
 			cout << "Ошибка: нарушение защиты памяти\n";
-			kill(getpid(), SIGINT);
+			exit(2);
 		}		
 		default: {
 			cout << "Ошибка: неизвестный сигнал\n";
@@ -31,22 +31,11 @@ int main (int argc, char *argv[]){
 	
 	if (signal_num == 8){
 		int a = 5/0;
-		pause();
-		sigaddset(&mask,SIGHUP);
-		sigprocmask(SIG_BLOCK, &mask, 0);
-		alarm(1);
-		pause();
-		cout << "Завершение процесса\n";
 	}
 
 	if (signal_num == 11){
 		main(0,NULL);	
-		pause();
-		sigaddset(&mask,SIGINT);
-		sigprocmask(SIG_BLOCK, &mask, 0);
-		alarm(1);
-		pause();
-		cout << "Завершение процесса\n";
 	}
 
+	return 0;
 }
--- Sixth lab/sixth_lab.cpp
+++ Sixth lab/sixth_lab.cpp
@@ -1,12 +1,16 @@
 #include <fstream>
+#include <ctime>
 
 using namespace std;
 
 int main(){
 
+	time_t seconds = time(NULL);
+	tm* timeinfo = localtime(&seconds);
+
 	ofstream fout;	
 	fout.open("crontab_output.txt", ios::app);
-	fout << "Тестовая строка\n";
+	fout << "Current time: " << asctime(timeinfo) << "\n";
 	fout.close();
 
 	return 0;
